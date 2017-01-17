abc9e255fd255bd3e9b16bb30c87f28ff5f024ea
Shubhankar Deshpande <shubhankardeshpande@hotmail.com>
2017-01-17 17:12:04 +0530

    Remove FLAGS condition for precalibration

--- precalibrate.py
+++ precalibrate.py
@@ -1,8 +1,6 @@
 def precalibrater(source_name):
-	lta_files = glob.glob('*.lta')
-	uvfits_files = glob.glob('*.UVFITS')
-	flag_files = glob.glob('*.FLAGS*')
-	for i in range(0,len(uvfits_files)):
-		if source_name in uvfits_files[i]:	
-			spam.pre_calibrate_targets(uvfits_files[i],flag_files[i])
-
+    uvfits_files = glob.glob('*.UVFITS')
+    #flag_files = glob.glob('*.FLAGS*')
+    for i in range(0,len(uvfits_files)):
+        if source_name in uvfits_files[i]:	
+            spam.pre_calibrate_targets(uvfits_files[i])
