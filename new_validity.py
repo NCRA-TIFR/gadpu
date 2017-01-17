aace7956091f10af19dfe9eaaf12aef8b0f9f579
Shubhankar Deshpande <shubhankardeshpande@hotmail.com>
2017-01-17 17:09:20 +0530

    Insert main body of code into function

--- new_validity.py
+++ new_validity.py
@@ -5,29 +5,27 @@ from sys import argv
 import os 
 
 def extract( file_name ):
-        with open(file_name) as f:
-                for i,line in enumerate(f,1):
-                        if "SCN" in line:
-                                return i
+    with open(file_name) as f:
+        for i,line in enumerate(f,1):
+            if "SCN" in line:
+                return i
+def main(lta_name):
+    os.system('ltahdr -i'+ lta_name + '> lta_file.txt')
+    dictionary = {}
+    #lta_file = str(argv[1])
+    skipped_rows = extract('lta_file.txt')-1
 
-os.system('ltahdr -i'+ argv[1]+ '> lta_file.txt')
-dictionary = {}
-#lta_file = str(argv[1])
-skipped_rows = extract('lta_file.txt')-1
+    header = pd.read_csv('lta_file.txt',skiprows=skipped_rows,delimiter=r"\s+")
+    flux = list(set(header["OBJECT"]))
+    #print flux
 
-header = pd.read_csv('lta_file.txt',skiprows=skipped_rows,delimiter=r"\s+")
-flux = list(set(header["OBJECT"]))
-print flux
+    header['Nrecs'] = header['Nrecs'].astype(float)
 
-header['Nrecs'] = header['Nrecs'].astype(float)
-
-for i in flux :
+    for i in flux :
         temp = header.loc[header.OBJECT==i,'Nrecs'].values
         temp = np.mean(temp)
         dictionary[i]=temp
-print dictionary
+    #print dictionary
 
-source = max(dictionary.iteritems(),key=operator.itemgetter(1))[0]
-print source
-
-
+    source = max(dictionary.iteritems(),key=operator.itemgetter(1))[0]
+    return source
