afe4e4739b27fc25667b54b05c1598ada888c1ba
Shubhankar Deshpande <shubhankardeshpande@hotmail.com>
2017-01-19 11:30:30 +0530

    It Works!

--- exec_thread_1.py
+++ exec_thread_1.py
@@ -1,6 +1,8 @@
-#import spam
+import spam
 import filter_lta
 import os
+import glob
+spam.set_aips_userid(11)
 #List of all directories containing valid observations 
 VALID_FILES = filter_lta.VALID_OBS()
 
@@ -8,10 +10,46 @@ VALID_FILES = filter_lta.VALID_OBS()
 THREAD_FILES = VALID_FILES[0:len(VALID_FILES):5]
 print 'Executing this thread'
 os.system('pwd')
-def main():
 
-    for i in THREAD_FILES:
-        LTA_FILES = os.chdir(i)
+def lta_to_uvfits():
+    os.chdir('fits/')
+    lta_files = glob.glob('*.lta*')
+    #flag_files = glob.glob('*.FLAGS*')
+    for i in range(len(lta_files)):
+        lta_file_name = lta_files[i]
+        uvfits_file_name = lta_file_name +'.UVFITS'
+        try:
+            spam.convert_lta_to_uvfits( lta_file_name, uvfits_file_name )
+        except RuntimeError:
+            #Write to log file
+            lta_to_uvfits_log.write()
+            continue
+    os.chdir('../')
+    return lta_files 
+
+def precalibrater():
+    uvfits_files = glob.glob('fits/*.UVFITS')
+    print uvfits_files
+    #flag_files = glob.glob('*.FLAGS*')
+    for i in range(0,len(uvfits_files)):
+        #if source_name in uvfits_files[i]:
+        #print source_name
+        os.system('pwd')
+        print('\n\n\nPrinting user ID AIPS\n\n\n')
+        print spam.get_aips_userid()
+        try:
+            spam.pre_calibrate_targets(uvfits_files[i])
+        except RuntimeError:
+            precalibrate_log.write()
+            continue
+    os.chdir('../')
+
+def main():     
+
+    #Copy all LTA files in valid obs dir to fits directory for processing
+        os.system('mv ' + THREAD_FILES + '/* ' + 'fits/')
+        lta_to_uvfits()
+        precalibrater()
         """
         #Convert the LTA file to the UVFITS format
         #Generates UVFITS file with same basename as LTA file 
@@ -27,3 +65,4 @@ def main():
         #datfil dir
         spam.process_target()
         """
+main()
--- parsel_wrapper.sh
+++ parsel_wrapper.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+start_parseltongue.sh THREAD11 11 /data2/shubhankar/gadpu/exec_thread_1.py 
+
+if false
+then
 i=11
-while [ $i -lt 17 ]
+while [ $i -lt 12 ]
 do
-	start_parseltongue.sh exec_thread_1.py $i THREAD_DIR_$[$i]
+	start_parseltongue.sh THREAD$i/ $vinay ../exec_thread_1.py 
 	i=$[$i+1]
 done
+fi
