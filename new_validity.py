1e6a424e2669441e6910d3a2803bc139df16dd51
Shubhankar Deshpande <shubhankardeshpande@hotmail.com>
2017-01-19 15:32:00 +0530

    Add scratch file for testing new validity

--- new_validity.py
+++ new_validity.py
@@ -9,23 +9,36 @@ def extract( file_name ):
         for i,line in enumerate(f,1):
             if "SCN" in line:
                 return i
-def main(lta_name):
-    os.system('ltahdr -i'+ lta_name + '> lta_file.txt')
+
+def main():
+    lta_file = str(argv[1])
+    calibrator_list = ['3C48', '3C147', '3C286']
+    os.system('ltahdr -i'+ lta_file + '> lta_file.txt')
     dictionary = {}
-    #lta_file = str(argv[1])
-    skipped_rows = extract('lta_file.txt')-1
+    try:
+        skipped_rows = extract('lta_file.txt')-1
 
-    header = pd.read_csv('lta_file.txt',skiprows=skipped_rows,delimiter=r"\s+")
-    flux = list(set(header["OBJECT"]))
-    #print flux
+        header = pd.read_csv('lta_file.txt',skiprows=skipped_rows,delimiter=r"\s+")
+        flux = list(set(header["OBJECT"]))
+        #print flux
 
-    header['Nrecs'] = header['Nrecs'].astype(float)
+        header['Nrecs'] = header['Nrecs'].astype(float)
 
-    for i in flux :
-        temp = header.loc[header.OBJECT==i,'Nrecs'].values
-        temp = np.mean(temp)
-        dictionary[i]=temp
-    #print dictionary
+        for i in flux :
+            temp = header.loc[header.OBJECT==i,'Nrecs'].values
+            temp = np.mean(temp)
+            dictionary[i]=temp
+        print dictionary
+    
+        #Sort the list of targets according to the number of recordings
+        list_of_targets = [ i for i,j in sorted(dictionary.iteritems(),key=operator.itemgetter(1), reverse=True)]
+        source = max(list_of_targets)
+        for i in len(flux): 
+            if source in calibrator_list:
+                continue
+            else:
+                return source
+    except:
+        pass 
 
-    source = max(dictionary.iteritems(),key=operator.itemgetter(1))[0]
-    return source
+print main()
