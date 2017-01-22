fbe479c2381023840dc57b7259fba915507efb00
Shubhankar Deshpande <shubhankardeshpande@hotmail.com>
2017-01-22 09:14:10 +0530

    Add functinality to spawn multiple processes

--- parsel_wrapper.sh
+++ parsel_wrapper.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
-start_parseltongue.sh THREAD11 11 /data2/shubhankar/gadpu/exec_thread_1.py 
+#start_parseltongue.sh THREAD11 11 /data2/shubhankar/gadpu/exec_thread_1.py 
 
-if false
-then
 i=11
-while [ $i -lt 12 ]
+while [ $i -lt 16 ]
 do
-	start_parseltongue.sh THREAD$i/ $vinay ../exec_thread_1.py 
+	start_parseltongue.sh THREAD$i/ $i ../exec_thread_$[$i-10].py &> THREAD$i.log &
 	i=$[$i+1]
 done
-fi
